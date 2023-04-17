b33a672a475133ba771fd66709521c3f43f48dbf
Yeeun Lee <leeyeah8245@gmail.com>
2023-04-17 11:07:37 +0900

    [Add Codes] 종속성 세팅

--- app/src/main/java/kr/co/yeeun/lee/demoi/jetpackroomexample/view/MainActivity.kt
+++ app/src/main/java/kr/co/yeeun/lee/demoi/jetpackroomexample/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package kr.co.yeeun.lee.demoi.jetpackroomexample
+package kr.co.yeeun.lee.demoi.jetpackroomexample.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.yeeun.lee.demoi.jetpackroomexample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
